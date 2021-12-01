bb049d3e3fc3a79da2ef26e5fe5a9956b1e60b70
Herman Westerman <hewe@posteo.org>
2021-12-01 10:32:53 +0100

    finish part one

--- src/day_01/main.py
+++ src/day_01/main.py
@@ -1,9 +1,22 @@
-
-
-
 def run():
-    pass
+    part_one()
+    part_two()
 
+
+def part_one():
+    my_input = open("input.txt")
+    number = int(my_input.readline())
+    count = 0
+    for line in my_input:
+        current = int(line)
+        if number < current:
+            count += 1
+        number = current
+    my_input.close()
+    print(f"Day 1, part 1 answer is: {count}")
+
+def part_two():
+        
 
 
 if __name__ == "__main__":
