94f0a4f77a8d302e0b8bbd40962da371ba1c8650
Herman Westerman <hewe@posteo.org>
2021-12-01 11:38:08 +0100

    add part twosolution

--- src/day_01/main.py
+++ src/day_01/main.py
@@ -1,10 +1,11 @@
-def run():
-    part_one()
-    part_two()
-
-
 def part_one():
     my_input = open("input.txt")
+    count = loop_through_input(my_input)
+    my_input.close()
+    print(f"Day 1, part 1 answer is: {count}")
+
+
+def loop_through_input(my_input):
     number = int(my_input.readline())
     count = 0
     for line in my_input:
@@ -12,11 +13,36 @@ def part_one():
         if number < current:
             count += 1
         number = current
-    my_input.close()
-    print(f"Day 1, part 1 answer is: {count}")
+    return count
+
 
 def part_two():
-        
+    my_input = open("input.txt")
+    my_input_list = [int(line) for line in my_input]
+    sum_list = []
+    try:
+        for index, i in enumerate(my_input_list):
+            current_sum = my_input_list[index] + my_input_list[index + 1] + my_input_list[index + 2]
+            sum_list.append(current_sum)
+    except IndexError:
+        loop_through_numbers(sum_list)
+
+
+def loop_through_numbers(sum_list):
+    count = 0
+    number = sum_list[0]
+    for num in sum_list[1:]:
+        current = num
+        if number < current:
+            count += 1
+        number = current
+
+    print(f"Day 1, part 2 answer: {count}")
+
+
+def run():
+    part_one()
+    part_two()
 
 
 if __name__ == "__main__":
