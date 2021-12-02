1e6589f259b94e0ce2b1e2341f1e9666ae638388
Herman Westerman <hewe@posteo.org>
2021-12-02 12:44:50 +0100

    add solutions no refactoring

--- src/day_02/main.py
+++ src/day_02/main.py
@@ -1,9 +1,48 @@
+def part_one():
+    my_map = {
+        "forward": [],
+        "down": [],
+        "up": [],
+    }
 
+    my_input = open("input.txt")
+    for i in my_input:
+        my_map[i.split()[0]].append(int(i.split()[1]))
+
+    my_input.close()
+    final_sum = sum(my_map['forward']) * (sum(my_map['down']) - sum(my_map['up']))
+    print(f"Day 2, part one answer is: {final_sum}")
+
+
+def part_two():
+    my_map = {
+        "forward": [],
+        "depth": 0,
+        "aim": 0,
+    }
+
+    my_input = open("input.txt")
+
+    for i in my_input:
+        if i.split()[0] == 'down':
+            my_map['aim'] += int(i.split()[1])
+        elif i.split()[0] == 'up':
+            my_map['aim'] -= int(i.split()[1])
+        elif i.split()[0] == 'forward':
+            if my_map['aim'] == 0:
+                my_map['depth'] += 0
+            else:
+                my_map['depth'] += (my_map['aim'] * int(i.split()[1]))
+
+            my_map['forward'].append(int(i.split()[1]))
+
+    final_sum = sum(my_map['forward']) * my_map['depth']
+    print(f"Day 2, part two answer is: {final_sum}")
 
 
 def run():
-    pass
-
+    part_one()
+    part_two()
 
 
 if __name__ == "__main__":
