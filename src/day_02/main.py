eaa3138f08fe268ffbd8992511d898dccb448f5d
Herman Westerman <hewe@posteo.org>
2021-12-02 17:45:08 +0100

    cleanup code

--- src/day_02/main.py
+++ src/day_02/main.py
@@ -5,14 +5,15 @@ def part_one():
         "up": [],
     }
 
-    my_input = open("input.txt")
-    for i in my_input:
-        my_map[i.split()[0]].append(int(i.split()[1]))
+    with open("input.txt", "r") as my_input:
+        for i in my_input:
+            my_map[i.split()[0]].append(int(i.split()[1]))
 
-    my_input.close()
     final_sum = sum(my_map['forward']) * (sum(my_map['down']) - sum(my_map['up']))
     print(f"Day 2, part one answer is: {final_sum}")
 
+
+# For a better solution, reuse map from part_one, however, I want to treat them as separate solutions.
 
 def part_two():
     my_map = {
@@ -21,20 +22,15 @@ def part_two():
         "aim": 0,
     }
 
-    my_input = open("input.txt")
-
-    for i in my_input:
-        if i.split()[0] == 'down':
-            my_map['aim'] += int(i.split()[1])
-        elif i.split()[0] == 'up':
-            my_map['aim'] -= int(i.split()[1])
-        elif i.split()[0] == 'forward':
-            if my_map['aim'] == 0:
-                my_map['depth'] += 0
-            else:
+    with open("input.txt", "r") as my_input:
+        for i in my_input:
+            if i.split()[0] == 'down':
+                my_map['aim'] += int(i.split()[1])
+            elif i.split()[0] == 'up':
+                my_map['aim'] -= int(i.split()[1])
+            elif i.split()[0] == 'forward':
                 my_map['depth'] += (my_map['aim'] * int(i.split()[1]))
-
-            my_map['forward'].append(int(i.split()[1]))
+                my_map['forward'].append(int(i.split()[1]))
 
     final_sum = sum(my_map['forward']) * my_map['depth']
     print(f"Day 2, part two answer is: {final_sum}")
