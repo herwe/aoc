f7a08fab2ecc7630e3423960e6f96081a62d6ba1
Herman Westerman <hewe@posteo.org>
2021-12-03 15:52:36 +0100

    add horrible part 2 solution

--- src/day_03/main.py
+++ src/day_03/main.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def first_part():
     gamma_rate_count = []
     epsilon_rate_count = []
@@ -35,9 +38,96 @@ def first_part():
     print(f"Day 3, part on answer is: {final_sum}")
 
 
-
 def second_part():
-    pass
+    counter_ones = 0
+    counter_zeros = 0
+    current = -1
+    with open("test.txt", "r") as my_input:
+        my_input_list = [i for i in my_input]
+
+    my_copy = copy.deepcopy(my_input_list)
+    count_list = []
+
+    join_characters(count_list, my_input_list)
+
+    my_input_list = rating_generator(count_list, counter_ones, counter_zeros, current, my_input_list)
+    my_copy = rating_generator_2(count_list, counter_ones, counter_zeros, current, my_copy)
+
+    final_sum = int(int(''.join(my_input_list), 2)) * (int(''.join(my_copy), 2))
+    print(f"Day 3, part two answer is: {final_sum}")
+
+
+def rating_generator(count_list, counter_ones, counter_zeros, current, my_input_list):
+    for index, i in enumerate(count_list):
+        for j in i:
+            if j == '1':
+                counter_ones += 1
+            elif j == '0':
+                counter_zeros += 1
+
+        if len(my_input_list) == 2:
+            current = '1'
+        else:
+            if counter_ones > counter_zeros:
+                current = '1'
+            elif counter_ones < counter_zeros:
+                current = '0'
+
+        counter_zeros = 0
+        counter_ones = 0
+
+        temp_list = []
+
+        for k_index, k in enumerate(my_input_list):
+            if k[index] != current:
+                temp_list.append(k)
+        my_input_list = [i for i in my_input_list if i not in temp_list]
+
+        if len(my_input_list) == 1:
+            return my_input_list
+
+        count_list = join_characters(count_list, my_input_list)
+
+
+def rating_generator_2(count_list, counter_ones, counter_zeros, current, my_input_list):
+    for index, i in enumerate(count_list):
+        for j in i:
+            if j == '1':
+                counter_ones += 1
+            elif j == '0':
+                counter_zeros += 1
+
+        if len(my_input_list) == 2:
+            current = '0'
+        else:
+            if counter_ones > counter_zeros:
+                current = '0'
+            elif counter_ones < counter_zeros:
+                current = '1'
+
+        counter_zeros = 0
+        counter_ones = 0
+
+        temp_list = []
+
+        for k_index, k in enumerate(my_input_list):
+            if k[index] != current:
+                temp_list.append(k)
+        my_input_list = [i for i in my_input_list if i not in temp_list]
+
+        if len(my_input_list) == 1:
+            return my_input_list
+
+        count_list = join_characters(count_list, my_input_list)
+
+
+def join_characters(count_list, my_input_list):
+    for index, _ in enumerate(range(5)):
+        temp = list(zip(*my_input_list))[index]
+        ''.join(temp)
+        count_list.append(''.join(temp))
+
+    return count_list
 
 
 def run():
