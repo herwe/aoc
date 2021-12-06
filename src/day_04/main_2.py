d822ebc75401711b8224180499b0d5a26ab6a787
Herman Westerman <hewe@posteo.org>
2021-12-06 16:11:02 +0100

    add nightmarish solution

---
+++ src/day_04/main_2.py
@@ -0,0 +1,121 @@
+bingo_numbers = []
+correct_marked = ('X', 'X', 'X', 'X', 'X')
+arr = []
+
+
+def part_two():
+    new_arr = []
+
+    with open("input.txt", "r") as file:
+        drawn_numbers = file.readline().split(',')
+
+    with open("input.txt", "r") as file:
+        lines = file.read()
+
+    lines = [line.replace(' ', 'BREAK') or line.replace('\n', 'BREAK') for line in lines]
+    lines = [line for x, line in enumerate(lines) if x > 290]  # 290 / # 71
+    checker = ''
+    for i in lines:
+        if i.isdigit():
+            checker += i
+        else:
+            new_arr.append(checker)
+            checker = ''
+
+    counter = 0
+    temp = []
+    for i in new_arr:
+        if i.isdigit():
+            temp.append(int(i))
+            counter += 1
+        if counter == 5:
+            bingo_numbers.append(temp)
+            counter = 0
+            temp = []
+
+    mark_bingo_tiles(drawn_numbers)
+
+
+
+
+def mark_bingo_tiles(drawn_numbers):
+    for number in drawn_numbers:
+        current_number = int(number)
+        for index, i in enumerate(bingo_numbers):
+            for inner_index, j in enumerate(i):
+                if current_number == j:
+                    number_to_be_marked = bingo_numbers[index][inner_index]
+                    bingo_numbers[index][inner_index] = "X"
+                    if check_if_bingo(number_to_be_marked, index):
+                        return
+
+
+def check_if_bingo(number_to_be_marked, index):
+    a = 0
+    b = 1
+    c = 2
+    d = 3
+    e = 4
+    lst = []
+
+    while True:
+        try:
+            lst += list(zip(bingo_numbers[a], bingo_numbers[b], bingo_numbers[c], bingo_numbers[d], bingo_numbers[e]))
+            a += 5
+            b += 5
+            c += 5
+            d += 5
+            e += 5
+        except IndexError:
+            break
+
+    board_count = 0
+    board_index = 0
+
+    if loop_and_mark(board_count, board_index, lst, number_to_be_marked):
+        return True
+
+    board_count = 0
+    board_index = 0
+    if loop_and_mark(board_count, board_index, bingo_numbers, number_to_be_marked):
+        return True
+
+
+def loop_and_mark(board_count, board_index, lst, number_to_be_marked):
+    for index, i in enumerate(lst):
+        if board_count == 25:
+            board_index += 1
+            board_count = 0
+        for j in range(5):
+            board_count += 1
+            if tuple(i) == correct_marked:
+                if calc_sum_of_winning_board(lst, board_index, number_to_be_marked):
+                    return True
+
+
+def calc_sum_of_winning_board(board, board_index, number_to_be_marked):
+    if board_index not in arr:
+        arr.append(board_index)
+
+    if len(arr) == len(board)/5:
+        my_start = board_index * 5
+        end = my_start + 5
+        end_sum = 0
+        for i in range(my_start, end):
+            for j in board[i]:
+                if j != 'X':
+                    end_sum += j
+        final_sum = end_sum * number_to_be_marked
+        print(f"Day 4, part two answer is {final_sum} ")
+        return True
+    else:
+        return False
+
+
+
+def run():
+    part_two()
+
+
+if __name__ == "__main__":
+    run()
