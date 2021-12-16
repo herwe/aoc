419cf7c5ce80eb85b954d39974f802196ff895b9
Herman Westerman <hewe@posteo.org>
2021-12-16 23:11:41 +0100

    add day1 && day2 solution

--- src/day_07/main.py
+++ src/day_07/main.py
@@ -1,9 +1,15 @@
 
 
-
 def run():
-    pass
+    print("This will take a few seconds.....")
+    with open("input.txt") as file:
+        input_list = [int(x) for x in file.read().strip().split(",")]
 
+    part_one = min([sum([max([x, i]) - min([x, i]) for x in input_list]) for i in range(min(input_list), max(input_list))])
+    part_two = min([sum([sum(range(1 + max([x, i]) - min([x, i]))) for x in input_list]) for i in range(min(input_list), max(input_list))])
+
+    print(f"Day 7, part one answer is: {part_one}")
+    print(f"Day 7, part two answer is: {part_two}")
 
 
 if __name__ == "__main__":
