bad0b9a041592ad913270eacb6dafc426b6512c3
Herman Westerman <hewe@posteo.org>
2022-12-05 20:16:04 +0100

    add second part

--- 2022/day_05/main.py
+++ 2022/day_05/main.py
@@ -1,35 +1,39 @@
-import re
-
 cargo = [['D', 'M', 'S', 'Z', 'R', 'F', 'W', 'N'], ['W', 'P', 'Q', 'G', 'S'], ['W', 'R', 'V', 'Q', 'F', 'N', 'J', 'C'],
          ['F', 'Z', 'P', 'C', 'G', 'D', 'L'], ['T', 'P', 'S'], ['H', 'D', 'F', 'W', 'R', 'L'], ['Z', 'N', 'D', 'C'],
          ['W', 'N', 'R', 'F', 'V', 'S', 'J', 'Q'], ['R', 'M', 'S', 'G', 'Z', 'W', 'V']]
 
 cargo2 = [['D', 'M', 'S', 'Z', 'R', 'F', 'W', 'N'], ['W', 'P', 'Q', 'G', 'S'], ['W', 'R', 'V', 'Q', 'F', 'N', 'J', 'C'],
-         ['F', 'Z', 'P', 'C', 'G', 'D', 'L'], ['T', 'P', 'S'], ['H', 'D', 'F', 'W', 'R', 'L'], ['Z', 'N', 'D', 'C'],
-         ['W', 'N', 'R', 'F', 'V', 'S', 'J', 'Q'], ['R', 'M', 'S', 'G', 'Z', 'W', 'V']]
-def swap_and_delete(commands):
-    #to_append = cargo[(commands[2]-1)]
-    #to_remove = cargo[(commands[1]-1)]
-    move = commands[0]
+          ['F', 'Z', 'P', 'C', 'G', 'D', 'L'], ['T', 'P', 'S'], ['H', 'D', 'F', 'W', 'R', 'L'], ['Z', 'N', 'D', 'C'],
+          ['W', 'N', 'R', 'F', 'V', 'S', 'J', 'Q'], ['R', 'M', 'S', 'G', 'Z', 'W', 'V']]
 
+def swap_and_delete(commands):
+    move = commands[0]
+    temp = []
     for i in range(move):
-        if cargo[(commands[1]-1)]:
-            cargo[(commands[2]-1)].append(cargo[(commands[1]-1)].pop())
-    #print(cargo[(commands[2]-1)])
-    #print(cargo[(commands[1]-1)])
-    #print(move)
-    #print(cargo)
-    print()
+        if cargo[(commands[1] - 1)]:
+            removed = cargo[(commands[1] - 1)].pop()
+            cargo[(commands[2] - 1)].append(removed)
+        if cargo2[(commands[1] - 1)]:
+            removed2 = cargo2[(commands[1] - 1)].pop()
+            temp.append(removed2)
+    for item in reversed(temp):
+        cargo2[(commands[2] - 1)].append(item)
 
 
 def part_one():
+    answer1 = ''
+    answer2 = ''
     with open("input.txt", "r") as f:
         for line in f.read().splitlines():
             commands = [int(s) for s in line.split() if s.isdigit()]
             swap_and_delete(commands)
         for container in cargo:
             if container:
-                print(container.pop(), end='')
+                answer1 = answer1 + container.pop()
+        for container in cargo2:
+            if container:
+                answer2 = answer2 + container.pop()
+        print(f"Day 5, part 1: {answer1}\nDay 5, part 2: {answer2}")
 
 if __name__ == "__main__":
     part_one()
