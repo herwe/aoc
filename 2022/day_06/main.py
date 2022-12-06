fd8d264449a8fed817c542a0a0fa3164d68c64dc
Herman Westerman <hewe@posteo.org>
2022-12-06 20:49:25 +0100

    add solution for both parts

--- 2022/day_06/main.py
+++ 2022/day_06/main.py
@@ -1,10 +1,12 @@
-
-
-
-def run():
-    pass
-
+def parts(part, marker):
+    with open("input.txt", "r") as f:
+        line = f.read()
+        for index, i in enumerate(line):
+            if len(set(line[(index - marker):index])) == marker:
+                print(f"Day 6, part {part} is : {index}")
+                break
 
 
 if __name__ == "__main__":
-    run()
+    parts(1, 4)
+    parts(2, 14)
