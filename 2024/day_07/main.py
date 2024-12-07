dd85e5ea67d4bab51d60ac4b36f12d000d9bfa67
herwe <58404234+herwe@users.noreply.github.com>
2024-12-07 15:50:52 +0100

    Day 7

--- 2024/day_07/main.py
+++ 2024/day_07/main.py
@@ -1,4 +1,72 @@
+from itertools import product
+
+
 def run():
+    part_one()
+    part_two()
+
+
+def part_one():
+    calibrations = {}
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            line = line.replace(":", "")
+            line = line.split()
+            calibrations[int(line[0])] = [int(x) for x in line[1:]]
+
+    test_value_sum = 0
+    for test_value in calibrations:
+        equation = calibrations[test_value]
+
+        operators = ['+', '*']
+        operator_combinations = list(product(operators, repeat=len(equation) - 1))
+
+        for ops in operator_combinations:
+            result = equation[0]
+            for num, op in zip(equation[1:], ops):
+                if op == '+':
+                    result += num
+                elif op == '*':
+                    result *= num
+            if result == test_value:
+                test_value_sum = test_value_sum + test_value
+                break
+
+    print(test_value_sum)
+
+
+def part_two():
+    calibrations = {}
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            line = line.replace(":", "")
+            line = line.split()
+            calibrations[int(line[0])] = [int(x) for x in line[1:]]
+
+    test_value_sum = 0
+    for test_value in calibrations:
+        equation = calibrations[test_value]
+
+        operators = ['+', '*', '||']
+        operator_combinations = list(product(operators, repeat=len(equation) - 1))
+
+        for ops in operator_combinations:
+            result = equation[0]
+            for num, op in zip(equation[1:], ops):
+                if op == '+':
+                    result += num
+                elif op == '*':
+                    result *= num
+                elif op == '||': # string concatenate operation, then convert back to a number
+                    tmp = list(str(result))
+                    tmp.append(str(num))
+                    result = int(''.join(tmp))
+            if result == test_value:
+                test_value_sum += test_value
+                break
+
+    print(test_value_sum)
+
 
 if __name__ == "__main__":
     run()
